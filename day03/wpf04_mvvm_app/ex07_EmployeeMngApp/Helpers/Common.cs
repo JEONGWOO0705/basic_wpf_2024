4fe63952f69b90d284224b68515442072c04c988
JW HWANG <kgwjddn544@gmail.com>
2024-05-03 16:31:10 +0900

    20240503 MVVM 완료

--- day03/wpf04_mvvm_app/ex07_EmployeeMngApp/Helpers/Common.cs
+++ day03/wpf04_mvvm_app/ex07_EmployeeMngApp/Helpers/Common.cs
@@ -5,9 +5,10 @@ namespace ex07_EmployeeMngApp.Helpers
     class Common
     {
         public static readonly string CONNSTRING = "Data Source=127.0.0.1;" +
-                                                   "Initial Catalog=EMS;" +
-                                                   "User ID=ems_user;" +
-                                                   "Encrypt=False" +
-                                                   "Password=ems_p@ss";
+                                                    "Initial Catalog=EMS;" +
+                                                    "Persist Security Info=True;" + 
+                                                    "User ID=ems_user;" +
+                                                    "Encrypt=False;" +
+                                                    "Password=ems_p@ss;";
     }
 }
