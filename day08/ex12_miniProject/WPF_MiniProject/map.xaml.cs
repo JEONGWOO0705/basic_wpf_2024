08842ba976154a5b97137dfc7383a62f33793688
JEONGWOO0705 <kgwjddn544@gmail.com>
2024-05-16 09:05:45 +0900

    Update map.xaml.cs

--- day08/ex12_miniProject/WPF_MiniProject/map.xaml.cs
+++ day08/ex12_miniProject/WPF_MiniProject/map.xaml.cs
@@ -11,7 +11,7 @@ namespace WPF_MiniProject
         {
             InitializeComponent();
         }
-        public map(string location_id) : this()
+        public map(string location_id) : this()     // 함수가 실행됬을때 부모의 함수(기존 생성자) 먼저 수행하게 된다.
         {
             BrsLoc.Address = $"https://map.naver.com/p/search/{location_id}";
         }
